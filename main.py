fc6b7333f6922e62fada953bcb02f33a69f28082
Thiago Ferraz <thiagovdferraz@gmail.com>
2024-10-13 21:57:53 -0300

    adicionado explicações do git

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print('Olá!')
+
+print("segunda alteração...")
+
+# git config --global user.name "Seu Nome"
+# git config --global user.email "seu.email@exemplo.com"
+# git config --global --list
+
+# git init
+# git status
+# git add file
+# git commit -m "mensagem"
+# git log
+# git restore file - volta para o último commit feito
